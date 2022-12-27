618d94887607a52d0f4265f8cf2918371ecf10f0
sharad2512 <shnarute95@gmail.com>
2022-12-27 11:44:46 +0530

    ADDED : Code To Swap Two Numbers Program

---
+++ src/SwapTwoNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SwapTwoNumber {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 45;
+        b = 3677;
+        c = a;
+
+        a = b;
+        b = c;
+        System.out.println("Swap Value of a : " + a);
+        System.out.println("Swap Value of b : " + b);
+    }
+}
